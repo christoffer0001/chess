a8d1557cb79cc0fda004588ca3a83fdf3a209aef
christoffer0001 <chbe0001@edu.mercantec.dk>
2026-04-07 16:37:05 +0200

    Made the queen class' move & capture logic, as a combination of rook and bishop.

--- class/Piece_Classes/Queen.js
+++ class/Piece_Classes/Queen.js
@@ -20,8 +20,52 @@ class Queen {
     }
   }
 
-  validMove() {
+  validMove(colour, pre, current) {
     let [px, py] = pre;
     let [cx, cy] = current;
+
+    this.diffX = Math.abs(cx - px);
+    this.diffY = Math.abs(cy - py);
+    this.dirX = px < cx ? 1 : -1;
+    this.dirY = py < cy ? 1 : -1;
+
+    if (this.diffX === 0) {
+      for (let i = 1; i <= this.diffY; i++) {
+        let piece = this.getPiece(px, py + i * this.dirY);
+        if (piece != "") {
+          if (this.captureCheck.check(colour, [cx, cy]) && py + i * this.dirY == cy) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      return true;
+    } else if (this.diffY === 0) {
+      for (let i = 1; i <= this.diffX; i++) {
+        let piece = this.getPiece(px + i * this.dirX, py);
+        if (piece != "") {
+          if (this.captureCheck.check(colour, [cx, cy]) && px + i * this.dirX == cx) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      return true;
+    } else if (this.diffX == this.diffY) {
+      for (let i = 1; i <= this.diffX; i++) {
+        let piece = this.getPiece(px + i * this.dirX, py + i * this.dirY);
+        this.tempPos = [px + i * this.dirX, py + i * this.dirY];
+        if (piece !== "") {
+          if (this.captureCheck.check(colour, this.tempPos) && i == this.diffX) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
   }
 }
